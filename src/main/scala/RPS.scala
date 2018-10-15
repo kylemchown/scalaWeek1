81a113f1270b901c4e5dd7143a8fae3457c0958c
kylemchown <kyle.m.chown@gmail.com>
2018-10-15 11:13:47 +0100

    Changed vars in rps to val arrays

--- src/main/scala/RPS.scala
+++ src/main/scala/RPS.scala
@@ -1,81 +1,78 @@
 object RPS{
   def main(args: Array[String]): Unit = {
-    rps(true)
+    rps(false)
   }
 
   def rps(player: Boolean): Unit ={
-    var playerWins = 0
-    var aiWins = 0
-    var playerRock = 0
-    var playerPaper = 0
-    var playerScissors = 0
+
+    val wins = Array(0,0)
+    val choices = Array(0,0,0)
     //var playerActions = scala.collection.mutable.ArrayBuffer.empty[Array[String]]
 
-    var ai1Wins = 0
-    var ai2Wins = 0
+    val aiwins = Array(0,0)
     if (player) {
       for (i <- 1 to 10) {
         val playerChoice = scala.io.StdIn.readLine("Enter your action as a string").toLowerCase.capitalize
-        val aiChoice = aiChoosePlayer(playerRock, playerPaper, playerScissors)
+        val aiChoice = aiChoosePlayer(choices(0), choices(1), choices(2))
 
 
         (playerChoice, aiChoice) match {
           case ("Rock", "Scissors") => println("AI plays Scissors")
-            playerWins += 1
-            playerRock += 1
+            wins(0) += 1
+            choices(0) += 1
             println("You win")
           case ("Scissors", "Paper") => println("AI plays Paper")
-            playerWins += 1
-            playerScissors += 1
+            wins(0) += 1
+            choices(2) += 1
             println("You win")
           case ("Paper", "Rock") => println("AI plays Rock")
-            playerWins += 1
-            playerPaper += 1
+            wins(0) += 1
+            choices(1) += 1
             println("You win")
           case ("Rock", "Rock") => println("AI plays Rock")
-            playerRock += 1
+            choices(0) += 1
             println("You draw")
           case ("Scissors", "Scissors") => println("AI plays Scissors")
-            playerScissors += 1
+            choices(2) += 1
             println("You draw")
           case ("Paper", "Paper") => println("AI plays Paper")
-            playerPaper += 1
+            choices(1) += 1
             println("You draw")
           case ("Rock", "Paper") => println("AI plays Paper")
-            aiWins += 1
-            playerRock += 1
+            wins(1) += 1
+            choices(0) += 1
             println("You lose")
           case ("Scissors", "Rock") => println("AI plays Rock")
-            aiWins += 1
-            playerScissors += 1
+            wins(1) += 1
+            choices(2) += 1
             println("You lose")
           case ("Paper", "Scissors") => println("AI plays Scissors")
-            aiWins += 1
-            playerPaper += 1
+            wins(1) += 1
+            choices(1) += 1
             println("You lose")
           case _ => println("You're input was invalid, so you wasted a game, good job")
         }
 
       }
-      println("You won " + playerWins + " time!")
+      println("You won " + wins(0) + " time!")
     }
     else{
       for(i<- 1 to 50) {
-        val aiChoice1 = aiChooseAI(playerRock, playerPaper, playerScissors)
-        val aiChoice2 = aiChooseAI(playerRock, playerPaper, playerScissors)
+        val aiChoice1 = aiChooseAI()
+        val aiChoice2 = aiChooseAI()
 
         (aiChoice1, aiChoice2) match {
           case ("Rock", "Scissors") => println("AI 1 plays Rock")
             println("AI 2 plays Scissors")
-            ai1Wins += 1
+            aiwins(0) += 1
             println("AI 1 win")
           case ("Scissors", "Paper") => println("AI 1 plays Scissors")
             println("AI 2 plays Paper")
-            ai1Wins += 1
+            aiwins(0) += 1
             println("AI 1 win")
           case ("Paper", "Rock") => println("AI 1 plays Paper")
             println("AI 2 plays Rock")
-            ai1Wins += 1
+            aiwins(0) += 1
             println("AI 1 win")
           case ("Rock", "Rock") => println("AI 1 plays Rock")
             println("AI 2 plays Rock")
@@ -88,21 +85,21 @@ object RPS{
             println("AI draw")
           case ("Rock", "Paper") => println("AI 1 plays Rock")
             println("AI 2 plays Paper")
-            ai2Wins += 1
+            aiwins(1) += 1
             println("AI 2 win")
           case ("Scissors", "Rock") => println("AI 1 plays Scissors")
             println("AI 2 plays Rock")
-            ai2Wins += 1
+            aiwins(1) += 1
             println("AI 2 win")
           case ("Paper", "Scissors") => println("AI 1 plays Paper")
             println("AI 2 plays Scissors")
-            ai2Wins += 1
+            aiwins(1) += 1
             println("AI 2 win")
         }
 
       }
-      println("AI 1 won " + ai1Wins + " time!")
-      println("AI 2 won " + ai2Wins + " time!")
+      println("AI 1 won " + aiwins(0) + " time!")
+      println("AI 2 won " + aiwins(1) + " time!")
 
     }
 
@@ -122,7 +119,7 @@ object RPS{
     }
   }
 
-  def aiChooseAI(rock: Int, paper: Int, scissors: Int): String ={
+  def aiChooseAI(): String ={
     val a = scala.util.Random
     val b = a.nextInt(3)
     b match{
